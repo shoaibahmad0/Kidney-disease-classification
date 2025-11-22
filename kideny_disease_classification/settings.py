872ac64fb87ba76c9ff03ebd6ac9fcc4b96dad2e
shoaibahmad0 <shoaibahmadbaig015@gmail.com>
2025-11-22 21:50:08 +0500

    first commit

--- kideny_disease_classification/settings.py
+++ kideny_disease_classification/settings.py
@@ -117,9 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# Add this line if it's not already present
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
